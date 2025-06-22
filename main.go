c3ff4221d5791762cfb2b0928d6ea4e5643a6ac5
kimaiyo <meshackkimaiyo5@gmail.com>
2025-06-22 22:08:32 +0300

    add GET endpoint for retrieving album by ID

--- main.go
+++ main.go
@@ -22,10 +22,22 @@ func postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+func getAlbumsId(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbumsId)
 
 	router.Run("localhost:8080")
 }
